d2a6657cc79bc03775a82347242e07c033077280
DeepanshuManocha <dmanocha1509@gmail.com>
2020-04-25 04:38:12 +0530

    Create Level

---
+++ Assets/Scripts/CreateLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+public class CreateLevel : MonoBehaviour
+{
+    [SerializeField]private GameObject inputField;
+    private string levelName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void StoreLevelName()
+    {
+        levelName= inputField.GetComponent<Text>().text;
+    }
+
+    public void AddNewScene()
+    {
+        StoreLevelName();
+        Scene newScene = SceneManager.CreateScene(levelName);
+        //var newScene = EditorSceneManager.NewScene(NewSceneSetup.DefaultGameObjects, NewSceneMode.Single);
+        Debug.Log("NewScene created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
