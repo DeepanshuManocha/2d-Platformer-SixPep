b390455ed53a9d24839b85b28a9ff06741e0a8d6
DeepanshuManocha <dmanocha1509@gmail.com>
2020-04-26 02:39:06 +0530

    start point & end point bug fixed

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -7,11 +7,14 @@ public class SpawnObjects : MonoBehaviour
 {
     public static GameObject spawnableObject=null;
     private GameObject spawnedObject=null;
+    public bool startPointSpawned, endPointSpawned;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPointSpawned = false;
+        endPointSpawned = false;
     }
 
     // Update is called once per frame
@@ -42,37 +45,46 @@ public class SpawnObjects : MonoBehaviour
                     return;
                 else if (hit)
                 {
-                    spawnedObject=Instantiate(spawnableObject, hit.point, Quaternion.identity) as GameObject;
+                    if(spawnableObject.CompareTag("Ground") || spawnableObject.CompareTag("Coin"))
+                        spawnedObject = Instantiate(spawnableObject, hit.point, Quaternion.identity) as GameObject;
+                    if(spawnableObject.CompareTag("startPoint") && !startPointSpawned)
+                        spawnedObject = Instantiate(spawnableObject, hit.point, Quaternion.identity) as GameObject;
+                    if (spawnableObject.CompareTag("endPoint") && !endPointSpawned)
+                        spawnedObject = Instantiate(spawnableObject, hit.point, Quaternion.identity) as GameObject;
+                   
+                    if (spawnedObject.CompareTag("startPoint"))
+                        startPointSpawned = true;
+                    if (spawnedObject.CompareTag("endPoint"))
+                        endPointSpawned = true;
+                    
                 }
-
             }
         }
     }
 
     void DestroyObjects()
     {
-        /*if (Input.GetMouseButtonDown(1))
-        {
-            if (spawnedObject == null)
-                return;
-            else if (spawnedObject.CompareTag("Ground") ||spawnedObject.CompareTag("Coin") || spawnedObject.CompareTag("startPoint") || spawnedObject.CompareTag("endPoint"))
-                Destroy(spawnedObject);
-           
-        }
-        */
-
         if (Input.GetMouseButtonDown(1))
         {
             var ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(ray, Camera.main.transform.forward);
 
-            if (hit.collider.CompareTag("Ground") || hit.collider.CompareTag("Coin") || hit.collider.CompareTag("startPoint") || hit.collider.CompareTag("endPoint"))
+            if (hit.collider.CompareTag("Ground") || hit.collider.CompareTag("Coin") )
                 Destroy(hit.collider.gameObject);
-                
-            else 
+           
+            else if(hit.collider.CompareTag("endPoint"))
             {
-                 return;
+                Destroy(hit.collider.gameObject);
+                endPointSpawned = false;
             }
+
+            else if (hit.collider.CompareTag("startPoint"))
+            {
+                Destroy(hit.collider.gameObject);
+                startPointSpawned = false;
+            }
+            else
+                return;
 
         }
     }
